7e972da1921bbc30deda1fccad2775ba7362bd09
direchlet <direchlet@gmail.com>
2012-10-21 01:23:46 -0500

    Initial Commit

---
+++ Carleton NNB/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Carleton NNB
+//
+//  Created by Shunji Li on 12-10-21.
+//  Copyright (c) 2012年 Shunji Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Carleton NNB/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Carleton NNB
+//
+//  Created by Shunji Li on 12-10-21.
+//  Copyright (c) 2012年 Shunji Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
